b0ee460b203a7678a10fb766d7585241b10e5b3e
Jing Xian <jingxian_98@hotmail.com>
2020-10-30 09:35:03 +0800

    Updated UI and Function

--- Activation Key Test.Designer.vb
+++ Activation Key Test.Designer.vb
@@ -22,9 +22,13 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'Button1
@@ -38,11 +42,12 @@ Partial Class Form1
         '
         'Label1
         '
-        Me.Label1.Location = New System.Drawing.Point(13, 96)
+        Me.Label1.Location = New System.Drawing.Point(13, 115)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(394, 171)
+        Me.Label1.Size = New System.Drawing.Size(394, 152)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "HTTP Respond"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'TextBox1
         '
@@ -51,16 +56,32 @@ Partial Class Form1
         Me.TextBox1.Size = New System.Drawing.Size(259, 20)
         Me.TextBox1.TabIndex = 2
         '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(86, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(259, 23)
+        Me.Label2.TabIndex = 3
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1500
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(419, 276)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Activation Key Tester"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -69,4 +90,6 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
 End Class
--- Activation Key Test.vb
+++ Activation Key Test.vb
@@ -3,10 +3,12 @@ Imports System.Net
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+        Label2.Text = "HTTP Request Sent!"
         Dim input As String = TextBox1.Text
         'Method 1
         'Address of URL
-        Dim URL As String = "https://server/activationkey?key="
+        Dim URL As String = "https://asia-southeast2-uhs-shop-93690.cloudfunctions.net/activationkey?key="
         Dim FullUrl As String = URL + input
         ' Get HTML data
         Dim client As WebClient = New WebClient()
@@ -14,12 +16,14 @@ Public Class Form1
         Dim reader As StreamReader = New StreamReader(data)
         Dim str As String = ""
         str = reader.ReadLine()
-        MsgBox(str)
-        Do
-            Console.WriteLine(str)
-            str = reader.ReadLine()
-            MsgBox(str)
-        Loop Until (str = "")
+        If str = "true" Then
+            Label1.Text = "Activation Key: " + input + Environment.NewLine + "Activated = true"
+        ElseIf str = "false" Then
+            Label1.Text = "Activation Key: Invalid" + Environment.NewLine + "Activated = false"
+        Else
+            MsgBox("Unable to communicate with server!")
+        End If
+        'MsgBox(str)
 
         '    'Method 2
         '    Try
@@ -38,6 +42,8 @@ Public Class Form1
         '    End Try
     End Sub
 
-
-
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label2.Text = ""
+        Timer1.Stop()
+    End Sub
 End Class
