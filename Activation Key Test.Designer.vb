e3822f65582860f8f5b8aa15b478dbdfac656425
Jing Xian <jingxian_98@hotmail.com>
2020-10-30 23:22:13 +0800

    Enhancement and Fix known bugs

--- Activation Key Test.Designer.vb
+++ Activation Key Test.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,76 +20,141 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(148, 39)
+        Me.Button1.Location = New System.Drawing.Point(148, 66)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(139, 54)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Send HTTP Request"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Label1
-        '
-        Me.Label1.Location = New System.Drawing.Point(13, 115)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(394, 152)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "HTTP Respond"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(86, 13)
+        Me.TextBox1.Location = New System.Drawing.Point(86, 44)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(259, 20)
         Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Enter Activation Key"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'Label2
         '
-        Me.Label2.Location = New System.Drawing.Point(86, 94)
+        Me.Label2.Location = New System.Drawing.Point(86, 124)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(259, 23)
         Me.Label2.TabIndex = 3
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Timer1
+        'Panel1
         '
-        Me.Timer1.Enabled = True
-        Me.Timer1.Interval = 1500
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.RadioButton2)
+        Me.Panel1.Controls.Add(Me.RadioButton1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(173, 25)
+        Me.Panel1.TabIndex = 4
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(90, 4)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(83, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Self Defined"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(2, 4)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(91, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "UHS Premium"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(176, 15)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(234, 20)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Text = "Enter your HTTP Link"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Panel2
+        '
+        Me.Panel2.AutoScroll = True
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(2, 150)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(417, 126)
+        Me.Panel2.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 3)
+        Me.Label1.MaximumSize = New System.Drawing.Size(407, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "HTTP Response"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(419, 276)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Activation Key Tester"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
-    Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label1 As Label
 End Class
--- Activation Key Test.vb
+++ Activation Key Test.vb
@@ -2,13 +2,64 @@
 Imports System.Net
 
 Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RadioButton1.Checked = True
+    End Sub
+
+    Dim errorcode As Integer
+    Dim counter As Integer
+    Dim input As String
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Timer1.Start()
+        counter = 0
+        Label1.Text = "HTTP Response"
         Label2.Text = "HTTP Request Sent!"
-        Dim input As String = TextBox1.Text
+        Label2.Text = "Reading......"
         'Method 1
         'Address of URL
-        Dim URL As String = "http://bit.ly/UHSPremium?key="
+        If RadioButton1.Checked = True Then
+            Selection1()
+        ElseIf RadioButton2.Checked = True Then
+            Selection2()
+        Else
+            MsgBox("Method Selection Error!")
+        End If
+        Label2.Text = "Done Reading!"
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        TextBox1.Text = "Enter Activation Key"
+        TextBox2.Enabled = False
+        Label1.Text = ""
+        Label2.Text = ""
+        TextBox2.Hide()
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        TextBox1.Text = "Enter your parameters (optional)"
+        TextBox2.Enabled = True
+        Label1.Text = ""
+        Label2.Text = ""
+        TextBox2.Show()
+    End Sub
+
+    Private Sub TextBox2_Click(sender As Object, e As EventArgs) Handles TextBox2.Click
+        If TextBox2.Text = "Enter your HTTP Link" Then
+            TextBox2.Text = ""
+        End If
+    End Sub
+
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        If TextBox1.Text = "Enter your parameters (optional)" Then
+            TextBox1.Text = ""
+        ElseIf TextBox1.Text = "Enter Activation Key" Then
+            TextBox1.Text = ""
+        End If
+    End Sub
+
+    Public Sub Selection1()
+        input = TextBox1.Text
+        Label2.Text = "Reading......"
+        Dim URL As String = "https://asia-southeast2-uhs-shop-93690.cloudfunctions.net/activationkey?key="
         Dim FullUrl As String = URL + input
         ' Get HTML data
         Dim client As WebClient = New WebClient()
@@ -16,34 +67,54 @@ Public Class Form1
         Dim reader As StreamReader = New StreamReader(data)
         Dim str As String = ""
         str = reader.ReadLine()
+        Console.WriteLine(str)
         If str = "true" Then
             Label1.Text = "Activation Key: " + input + Environment.NewLine + "Activated = true"
         ElseIf str = "false" Then
             Label1.Text = "Activation Key: Invalid" + Environment.NewLine + "Activated = false"
         Else
-            MsgBox("Unable to communicate with server!")
+            Label1.Text = "Unable to communicate with server!"
         End If
-        'MsgBox(str)
-
-        '    'Method 2
-        '    Try
-        '        Dim fr As System.Net.HttpWebRequest
-        '        Dim targetURI As New Uri("https://uhspremium.com/")
-
-        '        fr = DirectCast(HttpWebRequest.Create(targetURI), System.Net.HttpWebRequest)
-        '        If (fr.GetResponse().ContentLength > 0) Then
-        '            Dim str As New System.IO.StreamReader(fr.GetResponse().GetResponseStream())
-        '            Response.Write(str.ReadToEnd())
-        '            str.Close(); 
-        '       End If
-        '    Catch ex As System.Net.WebException
-        '        'Error in accessing the resource, handle it
-        '        MsgBox("Error")
-        '    End Try
+        Label2.Text = "Done Reading!"
     End Sub
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Label2.Text = ""
-        Timer1.Stop()
+    Public Sub Selection2()
+        Label2.Text = "Reading......"
+        Dim URL As String = TextBox2.Text
+        Dim FullUrl As String = URL + input
+        If TextBox1.Text = "Enter your parameters (optional)" Then
+            FullUrl = URL
+        End If
+        ' Get HTML data
+        Dim client As WebClient = New WebClient()
+        Dim data As Stream
+        Dim reader As StreamReader
+        Try
+            data = client.OpenRead(FullUrl)
+            reader = New StreamReader(data)
+        Catch ex As Exception
+            MsgBox("Error 404! Invalid Link.")
+            Return
+        End Try
+        Dim str As String
+        str = reader.ReadLine()
+        Console.WriteLine(str)
+        Label1.Text = str
+        Do While Not (str Is Nothing)
+            If Not str = "" Then
+                Dim GetText As String = Label1.Text
+                str = reader.ReadLine()
+                Console.WriteLine(str)
+                Label1.Text = GetText + Environment.NewLine + str
+            ElseIf counter > 10 Then
+                Exit Do
+            Else
+                Dim GetText As String = Label1.Text
+                str = reader.ReadLine()
+                Console.WriteLine(str)
+                Label1.Text = GetText + Environment.NewLine + str
+                counter = counter + 1
+            End If
+        Loop
     End Sub
 End Class
